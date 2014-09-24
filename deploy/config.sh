6b5d06ee44e8b833c7fc2edac8e8cee13c501e46
Sebastien Goasguen <runseb@gmail.com>
2014-09-24 18:17:11 +0200

    more overlay fixes

--- deploy/config.sh
+++ deploy/config.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-git clone https://github.com/runseb/kubernetes-exoscale.git
-cd kubernetes-exoscale/units
 find .
 
-ETCD_IP=`fleetctl list-machine | grep etcd | awk {'print $2'} | tail -n 1
+ETCD_IP=`fleetctl list-machines | grep etcd | awk {'print $2'} | tail -n 1`
 
 for f in `ls`;
 do
@@ -12,9 +10,12 @@ sed -i 's/<ip>/$ETCD_IP/g' $f
 fleetctl start $f
 done
 
-API_IP=`fleetctl list-units | grep apiserver | awk {'print $2'}
+API_IP=`fleetctl list-units | grep apiserver | awk {'print $2'}`
+IFS='/' read -ra IP <<< "$API_IP"
+echo ${IP[1]}
 
-echo API_IP
+wget -O ../kubecfg http://storage.googleapis.com/kubernetes/kubecfg
+chmod +x ../kubecfg
+export KUBERNETES_MASTER=http://${IP[1]}:8080
 
-wget http://storage.googleapis.com/kubernetes/kubecfg
-chmod +x ./kubecfg
+../kubecfg list /minions
